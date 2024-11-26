351489cc947dee4534029c38a9f26da1bbff47ca
Powerupsoft <138186195+powerupsoft@users.noreply.github.com>
2024-11-26 23:36:46 +0530

    Create slide2.ps1

---
+++ slide2.ps1
@@ -0,0 +1,19 @@
+## List all services
+
+    Get-Service
+
+## Start a service
+
+    Start-Service -Name "Spooler"
+
+## Stop a service
+
+    Stop-Service -Name "Spooler"
+
+## Restart a service
+
+    Restart-Service -Name "Spooler"
+
+## Check the status of a specific service
+
+    Get-Service -Name "Spooler" | Select-Object Status
